8cb23cbfad50b687734b434893079ade86d31163
DemosByYash <ygofficial88@gmail.com>
2025-07-29 12:25:00 +1200

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic;
+using WeatherApp.Models;
+
+namespace WeatherApp.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        
+        List<CityWeather> cities = new List<CityWeather>() {
+        new CityWeather() {
+            CityUniqueCode = "LDN",
+            CityName = "London",
+            DateAndTime = Convert.ToDateTime("2030-01-01 8:00"),
+            TemperatureFahrenheit = 33 },
+
+        new CityWeather() {
+            CityUniqueCode = "NYC",
+            CityName = "New York",
+            DateAndTime = Convert.ToDateTime("2030-01-01 3:00"),
+            TemperatureFahrenheit = 60 },
+
+        new CityWeather() {
+            CityUniqueCode = "PAR",
+            CityName = "Paris",
+            DateAndTime = Convert.ToDateTime("2030-01-01 9:00"),
+            TemperatureFahrenheit = 82 }
+        };
+
+        [Route("/")]
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Weather App - Home Page";
+            Response.StatusCode = 200; //OK
+            return View("Index", cities);
+        }
+
+        [Route("/weather/{cityCode}")]
+        public IActionResult CityView(string? cityCode)
+        {
+            if (cityCode != null)
+            {
+                foreach (CityWeather city in cities)
+                {
+                    if (city.CityUniqueCode == cityCode)
+                    {
+                        ViewData["Title"] = $"Weather App - {city.CityName}";
+                        Response.StatusCode = 200; // OK
+                        return View("CityView", city);
+                    }
+                }
+
+                // If no matching city is found
+                ViewData["Title"] = "Weather App - City Not Found";
+                Response.StatusCode = 404; // Not Found
+                return View("Error");
+            }
+
+            // If cityCode is null
+            ViewData["Title"] = "Weather App - Invalid Request";
+            Response.StatusCode = 400; // Bad Request
+            return View("Error");
+        }
+
+    }
+}
---
+++ Models/CityWeather.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using WeatherApp.Controllers;
+
+namespace WeatherApp.Models
+{
+    public class CityWeather
+    {
+        public string CityUniqueCode { get; set; }
+        public string CityName { get; set; }
+        public DateTime DateAndTime { get; set; }
+        public int TemperatureFahrenheit { get; set; }
+    }
+}
